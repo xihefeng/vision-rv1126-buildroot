c4b3c25ca4b157a62e7372913f47c076c58c4d71
nacuk <vitaliy.nimych@gmail.com>
2023-10-16 21:55:51 +0300

    vision-services: increment version to 0.0.11

--- buildroot/package/vision-app/vision-services/vision-services.mk
+++ buildroot/package/vision-app/vision-services/vision-services.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VISION_SERVICES_VERSION = 0.0.10
+VISION_SERVICES_VERSION = 0.0.11
 VISION_SERVICES_SITE = ssh://git@gitlab.hard-tech.org.ua/vision/vision-services.git
 VISION_SERVICES_SITE_METHOD = git
 VISION_SERVICES_INSTALL_STAGING = NO
