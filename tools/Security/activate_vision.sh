b3bfe13b5b42147397ab25b01500d468bd275a91
mlukyanenko <m.lukianenko@perenio.com>
2024-05-27 00:18:02 +0300

    activate_vision.sh, decrypt.sh: Fix the hexdump usage

--- tools/Security/activate_vision.sh
+++ tools/Security/activate_vision.sh
@@ -9,8 +9,8 @@ k=${2:-/tmp/vk}
 rm -f "$k"
 openssl rsautl -verify -inkey vision_public.pem -pubin -passin file:pass_pub.txt -in "$1" -out "$k" || exit 1
 
-k1=$(hexdump -e '/1 "%02x"' -n32 "$k")
-k2=$(hexdump -e '/1 "%02x"' -s32 "$k")
+k1=$(hexdump -v -e '/1 "%02x"' -n32 "$k")
+k2=$(hexdump -v -e '/1 "%02x"' -s32 "$k")
 TAR_FN="/tmp/vs.tar.gz"
 openssl aes-256-cbc -d -K "$k1" -iv "$k2" -in "/oem/assets/vs.bin" -out "$TAR_FN" || exit 2
 
