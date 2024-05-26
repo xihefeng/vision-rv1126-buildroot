81ee37eead08fcb910081cce8f84f20b3b53aa5c
mlukyanenko <m.lukianenko@perenio.com>
2024-05-26 23:26:48 +0300

    encrypt.sh: Fix the hexdump usage

--- tools/Security/encrypt.sh
+++ tools/Security/encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl aes-256-cbc -salt -K $(hexdump -e '/1 "%02x"' -n32 vision.key) -iv $(hexdump -e '/1 "%02x"' -s32 vision.key) -in "$1" -out "$2"
+openssl aes-256-cbc -salt -K $(hexdump -v -e '/1 "%02x"' -n32 vision.key) -iv $(hexdump -v -e '/1 "%02x"' -s32 vision.key) -in "$1" -out "$2"
